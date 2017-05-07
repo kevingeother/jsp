1ba49acf0354de92fd6580bc793e99be3db6a703
gogohan <binuswaraj@gmail.com>
2017-05-07 16:16:13 +0530

    output

--- plotter.py
+++ plotter.py
@@ -9,12 +9,20 @@ doOpen = True
 def getStr(machineJob, ts=0):
     if machineJob == -1:
         return "End '" + str(ts) + "'"
+    #return str(machineJob[0])
     if len(machineJob[-1]) > 0:
         return str(machineJob)
     else:
         return str(machineJob[:-1])
 
+def decimal(floatVal):
+    return float("{0:.2f}".format(floatVal))
+
 def drawDag(G, S, I, ts, s):
+    f = open('output', 'wb')
+    f.write("JOB ID, MACHINE\n")
+    for l in s:
+        f.write(str(l)+'\n')
     A = gv.Digraph(format='svg')
     machines = list(set([x[2] for x in S]))
     file_resource = [[res for res in I[s[i][0]][1]] for i in xrange(len(s))]
@@ -34,10 +42,13 @@ def drawDag(G, S, I, ts, s):
     for edge in edges:
         A.edge(edge[0],edge[1])
     A.render('dag')
-    print s
     noFileS=[x[:-1] for x in S]
     noFileS.sort(key=lambda tup: tup[1] + tup[3])
-    print [(s[0], s[1]+s[3]) for s in noFileS]
+    noFileS=[(s[0], decimal(s[1]+s[3]), s[1]) for s in noFileS]
+    f.write("\n\nJOBID, FINISHING TIME, STARTING TIMES\n")
+    for l in noFileS:
+        f.write(str(l) + '\n')
+    f.close()
     if doOpen:
         os.system('xdg-open dag.svg')
 
